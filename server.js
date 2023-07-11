b03d2f1e6edcc98ae98b18fb3153c8d105299fe3
nilkamal-ilmiya <nilkamal@ilmiya.com>
2023-07-11 23:20:51 +0530

    change file name

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const app = require("./index")
+const app = require("./app")
 const PORT = process.env.NODE_PORT || 5000
 
 
-app.listen(PORT,"0.0.0.0", () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Example app listening at port http://localhost:${PORT}`)
 })
